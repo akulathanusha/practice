7a633704b65210a58867eb9179f91369fc057545
akulathanusha <thanushaakula@gmail.com>
2023-11-09 11:35:58 +0530

    Create DecrementingFor.java

---
+++ decrementingfor/DecrementingFor.java
@@ -0,0 +1,13 @@
+package decrementingfor;
+import java.util.Scanner;
+public class DecrementingFor {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Your Range");
+		int num = scan.nextInt();
+		for(int i=num; i>=1; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
